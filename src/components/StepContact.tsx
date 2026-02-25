cf4b0e30222ab4063f2f4a2a8adadce42da11f1e
Link <dsi@sunlib.fr>
2026-02-25 07:59:05 +0100

    Updated StepContact.tsx

--- src/components/StepContact.tsx
+++ src/components/StepContact.tsx
@@ -210,7 +210,7 @@ export const StepContact: React.FC<StepContactProps> = ({
             ) : (
               <>
                 <CheckCircle className="w-5 h-5 mr-2" />
-                Envoyer ma demande
+                Envoyer ma demande et voir mes économies
               </>
             )}
           </button>
